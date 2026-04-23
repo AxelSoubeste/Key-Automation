6ce8249ce8c60c1711bc93d9ddebbf7a6e14e727
AxelS <axelsoubestte@gmail.com>
2026-04-23 08:30:05 -0300

    Finalizando pruebas

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Prueba 2");
+﻿string name1 = "Axel";
+string name2 = "Tomas";
+
+Console.WriteLine(name1 + " y " + name2);
+Console.WriteLine(" son los dueños de este proyecto");
